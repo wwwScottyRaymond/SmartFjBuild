de4b83dcd1c7bec68a53d5eca64c1aeec30a7cc7
Scotty Raymond <scotty@Scottys-MacBook-Pro.local>
2024-10-07 21:26:15 -0600

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import HomePage from "./smartFjHomePage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <h1>This is the notes for the annotated bib</h1>
+        <h3>Main Concepts:</h3>
+        <ul>
+          <li>Who,What, Where, When, Where, Why,</li>
+          <li>Get a list scholarship articles.</li>
+        </ul>
+        <HomePage />
+      </main>
     </div>
   );
 }
---
+++ src/smartFjHomePage.js
@@ -0,0 +1,19 @@
+function HomePage(){
+    return( 
+        <div>
+            <header>
+                <h1>Welcome to the Smart FJ Build</h1>
+            </header>
+            <main>
+                <ul>
+                    <p>Most Recent Update:</p>
+                    <li><p className="MRU">The ELM327 arrives tommorrow Mon Oct 7</p></li>
+                </ul>
+            </main>
+            <footer>
+            </footer>
+        </div>
+    );
+};
+
+export default HomePage;
