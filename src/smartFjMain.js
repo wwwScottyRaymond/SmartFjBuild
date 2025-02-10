88242c0211ea08b864bf313bc8c9d2b2f3f56e3d
Scotty Raymond <Kotascotty@gmail.com>
2025-02-10 14:02:09 -0700

    Need to add backend to form

--- src/smartFjMain.js
+++ src/smartFjMain.js
@@ -2,18 +2,7 @@ function MainSection(){
     return( 
         <div>
             <main>
-                <img src="/Frank.HEIC"></img>
-                <ul>
-                    <p>Most Recent Update:</p>
-                    <li>
-                        <p className="MRU">The ELM327 arrives tommorrow Mon Oct 7</p>
-                        </li>
-                </ul>
-                <ul>
-                <p>Stay tuned...</p>
-                    <li>
-                    </li>
-                </ul>
+
             </main>
             <footer>
             </footer>
